4151c6cd90a19bad7956e05eef0e478365000d87
Leticia Dupont <leticia.dupont20@gmail.com>
2024-11-04 11:28:55 -0300

    update

--- test/app.test.js
+++ test/app.test.js
@@ -2,10 +2,27 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../src/app');
 const expect = chai.expect;
+const server = require('../src/app'); // Import the server
 
 chai.use(chaiHttp);
 
+let serverInstance;
+
 describe('GET /', () => {
+  before((done) => {
+    serverInstance = server.listen(3000, () => {
+      console.log('Test server running on http://localhost:3000');
+      done();
+    });
+  });
+
+  after((done) => {
+    serverInstance.close(() => {
+      console.log('Test server stopped');
+      done();
+    });
+  });
+
   it('should return welcome message', (done) => {
     chai.request(app)
       .get('/')
